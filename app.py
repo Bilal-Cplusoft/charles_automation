d00c752b90ce2310afab077b151205717fb65c78
Bilal Akhtar <bilal.akhter@cplusoft.co>
2026-08-28 16:13:20 +0500

    fix grid lining***

--- app.py
+++ app.py
@@ -342,63 +342,9 @@ def generate_spot_grid_requests(grid_format, new_sheet_id):
                     })
                     updates.append({"range": gspread.utils.rowcol_to_a1(r1+1, c1_r+1), "values": [["Bankrupt"]]})
 
-    elif grid_format == "10_spot":
-        for i in range(10):
-            r1 = 2 + i
-            r2 = r1 + 1
-            reqs.append({
-                "mergeCells": {
-                    "range": {
-                        "sheetId": new_sheet_id,
-                        "startRowIndex": r1,
-                        "endRowIndex": r2,
-                        "startColumnIndex": 2,
-                        "endColumnIndex": 12
-                    },
-                    "mergeType": "MERGE_ALL"
-                }
-            })
-            reqs.append({
-                "mergeCells": {
-                    "range": {
-                        "sheetId": new_sheet_id,
-                        "startRowIndex": r1,
-                        "endRowIndex": r2,
-                        "startColumnIndex": 18,
-                        "endColumnIndex": 28
-                    },
-                    "mergeType": "MERGE_ALL"
-                }
-            })
-
-    elif grid_format == "5_spot":
-        for i in range(5):
-            r1 = 2 + i * 2
-            r2 = r1 + 2
-            reqs.append({
-                "mergeCells": {
-                    "range": {
-                        "sheetId": new_sheet_id,
-                        "startRowIndex": r1,
-                        "endRowIndex": r2,
-                        "startColumnIndex": 2,
-                        "endColumnIndex": 12
-                    },
-                    "mergeType": "MERGE_ALL"
-                }
-            })
-            reqs.append({
-                "mergeCells": {
-                    "range": {
-                        "sheetId": new_sheet_id,
-                        "startRowIndex": r1,
-                        "endRowIndex": r2,
-                        "startColumnIndex": 18,
-                        "endColumnIndex": 28
-                    },
-                    "mergeType": "MERGE_ALL"
-                }
-            })
+    elif grid_format in ["10_spot", "5_spot"]:
+        # Keep 10 columns unmerged so vertical lines aligned under top digits 0-9 remain intact
+        pass
 
     elif grid_format == "50_spot":
         for r_i in range(10):
